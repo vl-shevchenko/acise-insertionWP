cf1a54a6a791306eed9b3b2fb302e9cccdbcf8c3
Vlad <6847864@gmail.com>
2023-01-06 23:27:25 +0200

    added section fqa

--- js/index.js
+++ js/index.js
@@ -6,3 +6,20 @@ var swiper = new Swiper('.mySwiper', {
     clickable: true,
   },
 });
+
+const fqaSectionQuestionsWrapper = document.querySelector(
+  '.fqa__section-questions-wrapper'
+);
+const fqaQuestionWrapper = document.querySelectorAll(
+  'div.fqa__question-wrapper'
+);
+
+fqaSectionQuestionsWrapper.addEventListener('click', showAnswer);
+function showAnswer(e) {
+  let dotsCurrent = e.target;
+  if (e.target.matches('.fqa__question-wrapper')) {
+    for (let i = 0; i < fqaQuestionWrapper.length; i++) {
+      dotsCurrent.classList.toggle('open');
+    }
+  }
+}
