9b9d6193cc433b9776afad765e03cf8b3d4fabd0
Vlad <6847864@gmail.com>
2023-01-09 20:57:42 +0200

    added responsive design and fixed bug

--- js/index.js
+++ js/index.js
@@ -1,33 +1,59 @@
-var swiper = new Swiper('.mySwiper', {
-  spaceBetween: 30,
+//swiper
+let swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 17,
+  slidesPerGroup: 3,
   loop: true,
+  loopFillGroupWithBlank: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    enabled: false,
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1400: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
 });
 
+const fqaQuestion = document.querySelectorAll('p.fqa__question');
 const fqaSectionQuestionsWrapper = document.querySelector(
   '.fqa__section-questions-wrapper'
 );
 const fqaQuestionWrapper = document.querySelectorAll(
   'div.fqa__question-wrapper'
 );
-
-fqaSectionQuestionsWrapper.addEventListener('click', showAnswer);
-function showAnswer(e) {
-  let dotsCurrent = e.target;
-  if (e.target.matches('.fqa__question-wrapper')) {
-    for (let i = 0; i < fqaQuestionWrapper.length; i++) {
-      dotsCurrent.classList.toggle('open');
-    }
-  }
-}
-
 const contactUsButton = document.querySelector('.contact-us__button');
 const formContainer = document.querySelector('.form-container');
 const formClose = document.querySelector('.form-close');
+const burger = document.querySelector('#burger');
+const popup = document.querySelector('#popup');
 
+//fqa accordion
+fqaSectionQuestionsWrapper.addEventListener('click', showAnswer);
+function showAnswer(e) {
+  let dotsCurrent = e.target;
+
+  for (let i = 0; i < fqaQuestion.length; i++) {
+    dotsCurrent.closest('.fqa__question-wrapper').classList.toggle('open');
+  }
+}
+
+//form
 contactUsButton.addEventListener('click', function (e) {
   formContainer.classList.add('form-open');
 });
@@ -35,9 +61,7 @@ formClose.addEventListener('click', function (e) {
   formContainer.classList.remove('form-open');
 });
 
-const burger = document.querySelector('#burger');
-const popup = document.querySelector('#popup');
-
+//mobile menu
 burger.addEventListener('click', burgerHandler);
 function burgerHandler(e) {
   popup.classList.toggle('open');
